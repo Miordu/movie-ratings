5ec64f202c54e5f55e3884571bb90e2282f78793
Quinntin Saint Laurent <qysl10@gmail.com>
2025-04-19 14:56:02 -0400

    Complete Part 2: Add CRUD operations and seed database

--- crud.py
+++ crud.py
@@ -1,10 +1,13 @@
 """CRUD operations."""
-
 from model import db, User, Movie, Rating, connect_to_db
 
 def create_user(email, password):
     """Create and return a new user."""
     user = User(email=email, password=password)
+    
+    db.session.add(user)
+    db.session.commit()
+    
     return user
 
 def create_movie(title, overview, release_date, poster_path):
@@ -15,15 +18,20 @@ def create_movie(title, overview, release_date, poster_path):
         release_date=release_date,
         poster_path=poster_path,
     )
-
+    
+    db.session.add(movie)
+    db.session.commit()
+    
     return movie
 
-def create_rating(user, movie, score):
-    """Create and return a new rating."""
-    rating = Rating(user=user, movie=movie, score=score)
-    return rating
+def get_movies():
+    """Return all movies."""
+    return Movie.query.all()
+
+def get_movie_by_id(movie_id):
+    """Return a movie by primary key."""
+    return Movie.query.get(movie_id)
 
 if __name__ == '__main__':
-    from model import connect_to_db
-    connect_to_db()
-
+    from server import app
+    connect_to_db(app)
